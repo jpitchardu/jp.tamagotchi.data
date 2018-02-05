a444bccf39c86d7405cfe390de73bb317d1d090f
J. Pichardo <j.pichardo@tuta.io>
2018-02-04 20:34:15 -0800

    Initial Structure

---
+++ src/jp.tamagotchi.data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jp.tamagotchi.data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
