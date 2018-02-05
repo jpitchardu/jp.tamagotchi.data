9bd05ebc94c557f9dd3ab29ff74eb3d3f5a2b3c5
J. Pichardo <j.pichardo@tuta.io>
2018-02-04 22:05:25 -0800

    Server

--- src/jp.tamagotchi.data/Program.cs
+++ src/jp.tamagotchi.data/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
-namespace jp.tamagotchi.data
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace jp.tamagotchi.data {
+    class Program {
+        static void Main(string[] args) {
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json", true, true)
+                .Build();
+
+            var server = new Server(configuration);
+
+            server.Start();
+
+            server.Stop().Wait();
+
         }
     }
 }
---
+++ src/jp.tamagotchi.data/Server.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace jp.tamagotchi.data {
+    public class Server {
+
+        private IConfiguration _configuration;
+        private IConfigurationSection _serverConfiguration;
+        private Grpc.Core.Server _server;
+
+        public Server (IConfiguration configuration) {
+            _configuration = configuration;
+            _serverConfiguration = _configuration.GetSection ("server");
+
+            BuildServer ();
+
+        }
+
+        private void BuildServer () {
+
+            var host = _serverConfiguration["host"];
+            var port = int.Parse (_serverConfiguration["port"]);
+            var credentials = Grpc.Core.ServerCredentials.Insecure;
+
+            _server = new Grpc.Core.Server {
+                Ports = { new Grpc.Core.ServerPort (host, port, credentials) }
+            };
+
+        }
+
+        public void Start () => _server.Start ();
+
+        public Task Stop () => _server.ShutdownAsync ();
+
+    }
+}
